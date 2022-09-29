f4bd4551e5fe84c012e6ec2c4f7aee96f512dae4
Nansi Uzunova <nansi.uzunova@imperiaonline.org>
2022-09-29 15:59:11 +0300

    Using stateless widget to perform hot reload.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp() : super(key: null);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
+
