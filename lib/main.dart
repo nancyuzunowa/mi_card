4502f32c3999602c20cd79900c8f6df4b1253338
Nansi Uzunova <nansi.uzunova@imperiaonline.org>
2022-10-07 17:08:31 +0300

    Added phone and email info.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,22 +19,69 @@ class MyApp extends StatelessWidget {
                 radius: 50.0,
                 foregroundImage: AssetImage("images/avatar.png"),
               ),
-              Text("Nancy Uzunova",
+              Text(
+                "Nancy Uzunova",
                 style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
               ),
+              Text(
+                'MOBILE DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 25.0,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
               ),
-              Text('MOBILE DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                fontSize: 25.0,
-                color: Colors.teal.shade100,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold
-              ),)
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "+35911111111",
+                      style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                          color: Colors.teal),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "andgivj@abv.bg",
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal,
+                          fontFamily: 'Source Sans Pro'),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
@@ -44,4 +89,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
