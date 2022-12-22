36343a9a0ebcf3779909b60c7067d27d1b006bd2
Nansi Uzunova <nansi.uzunova@imperiaonline.org>
2022-12-22 13:38:26 +0200

    Using listtile and Card classes

--- lib/main.dart
+++ lib/main.dart
@@ -36,51 +36,39 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2.5,
                     fontWeight: FontWeight.bold),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
+              Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 color: Colors.white,
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      "+35911111111",
-                      style: TextStyle(
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
-                          color: Colors.teal),
-                    )
-                  ],
-                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+35911111111",
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal),
+                  ),
+                )
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
+              Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 color: Colors.white,
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      "andgivj@abv.bg",
-                      style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.teal,
-                          fontFamily: 'Source Sans Pro'),
-                    ),
-                  ],
-                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "andgivj@abv.bg",
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                )
               )
             ],
           ),
